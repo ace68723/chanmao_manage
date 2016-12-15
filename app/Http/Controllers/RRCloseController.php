65b74d9417e84822c57e8131800be823d888068a
Gavin Gan <Gavin@Big-Machine.hitronhub.home>
2016-12-15 16:14:42 -0500

    feat(rr_role): added validation on parameters

--- app/Http/Controllers/RRCloseController.php
+++ app/Http/Controllers/RRCloseController.php
@@ -56,23 +56,33 @@
 
 			}
 
-			else{
-				
-				/* Number of parameter exceeded 3 */
-				if(sizeof($request->all()) != 3){
+			elseif(sizeof($request->all()) != 3){
 					
-					$ev_result = 1;
-					$error_code = 401;
-					$error_msg = 'Number of Invalid amount of arguments';
-				}
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Number of Invalid amount of arguments';
 
-				else{
+			}
 
-					DB::statement('CALL insert_rrclose(?, ?, ?)', array_values($request->all()));
-
-				}
+			elseif(!array_key_exists("rid", $request->all()) || !array_key_exists("start_time", $request->all()) || !array_key_exists("end_time", $request->all())){
 				
-		    }
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Field name incorrect';
+
+			}
+
+			elseif(!is_int($request->rid) || !is_string($request->start_time) || !is_string($request->end_time)) {
+				
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Field type incorrect';	
+
+			}
+
+			else{
+				DB::statement('CALL insert_rrclose(?, ?, ?)', array_values($request->all()));
+			}
 
 		    $response = [
 			  	'ev_result' => $ev_result,
@@ -97,24 +107,34 @@
 				$error_code = 401;
 
 			}
-			else{
 
-				/* Number of parameter exceeded 3 */
-				if(sizeof($request->all()) != 4){
+			elseif(sizeof($request->all()) != 4){
 					
-					$ev_result = 1;
-					$error_code = 401;
-					$error_msg = 'Number of Invalid amount of arguments';
-				}
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Number of Invalid amount of arguments';
 
+			}
 
-				else{
+			elseif(!array_key_exists("rid", $request->all()) || !array_key_exists("start_time", $request->all()) || !array_key_exists("end_time", $request->all()) || !array_key_exists("id", $request->all())){
+				
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Field name incorrect';
 
-			    	//$update_array = array_merge(array_values($request->all()), array($close_id));
-					DB::statement('CALL update_rrclose(?, ?, ?, ?)', array_values($request->all()));
-		  		}
-	        
-	        }
+			}
+
+			elseif(!is_int($request->rid) || !is_string($request->start_time) || !is_string($request->end_time) || !is_int($request->id) ) {
+				
+				$ev_result = 1;
+				$error_code = 401;
+				$error_msg = 'Field type incorrect';	
+
+			}
+
+			else{
+				DB::statement('CALL update_rrclose(?, ?, ?, ?)', array_values($request->all()));
+			}
 
 	        $response = [
 			  	'ev_result' => $ev_result,
